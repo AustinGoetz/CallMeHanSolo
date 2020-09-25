27336ae234c1a62c90a59a65ea31fb4f13eb3b71
Austin Goetz <a.goetz09@gmail.com>
2020-09-25 00:24:02 -0600

    adds character model object

---
+++ SWAPISolo/SWAPISolo/Models/Character.swift
@@ -0,0 +1,20 @@
+//
+//  Character.swift
+//  SWAPISolo
+//
+//  Created by Austin Goetz on 9/25/20.
+//
+
+import Foundation
+
+struct Character {
+    let name: String
+    let height: String
+    let mass: String
+    let hair_color: String
+    let skin_color: String
+    let eye_color: String
+    let birth_year: String
+    let gender: String
+    let homeworld: String
+}
